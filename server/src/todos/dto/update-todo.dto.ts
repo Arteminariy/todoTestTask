7927dd402c0215cf20151278cfc6f401f6cef5e7
Arteminariy <artemborisoff.ab@gmail.com>
2023-06-27 01:35:16 +0300

    Добавил возможность изменять задачи

--- server/src/todos/dto/update-todo.dto.ts
+++ server/src/todos/dto/update-todo.dto.ts
@@ -1,10 +1,4 @@
-import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
+import { PartialType } from '@nestjs/swagger';
 import { CreateTodoDto } from './create-todo.dto';
-import { IsBoolean, IsNotEmpty } from 'class-validator';
 
-export class UpdateTodoDto extends PartialType(CreateTodoDto) {
-  @IsNotEmpty({ message: 'Не должно быть пустым' })
-  @IsBoolean({ message: 'Должно быть логическим типом' })
-  @ApiPropertyOptional({ description: 'Выполнена ли задача', example: 'true' })
-  isDone?: boolean;
-}
+export class UpdateTodoDto extends PartialType(CreateTodoDto) {}
