842f6a5b06449502b7f76970847c5b168c63d271
Arteminariy <artemborisoff.ab@gmail.com>
2023-06-26 19:30:59 +0300

    logout

--- client/src/pages/MainPage/MainPage.tsx
+++ client/src/pages/MainPage/MainPage.tsx
@@ -1,17 +1,29 @@
 import { FC } from 'react';
 import styles from './MainPage.module.scss';
 import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { RootState } from '../../store/store';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../store/store';
 import TodoList from './components/TodoList/TodoList';
+import { Button } from 'antd';
+import { logout } from '../../store/slices/authSlice';
 
 const MainPage: FC = () => {
-	const {isAuth} = useSelector((state: RootState) => state.auth)
+	const { isAuth } = useSelector((state: RootState) => state.auth);
+
+	const dispatch = useDispatch<AppDispatch>();
+
+	const handleLogout = () => {
+		dispatch(logout())
+	}
+
 	return (
 		<div>
 			<h1 className={styles.title}>Список дел</h1>
 			{isAuth ? (
-				<TodoList/>
+				<>
+					<TodoList />
+					<Button className={styles.logout} type="primary" danger onClick={handleLogout}>Выйти из аккаунта</Button>
+				</>
 			) : (
 				<>
 					<Link className={styles.link} to="/login">
