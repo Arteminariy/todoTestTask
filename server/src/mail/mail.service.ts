1fd95976471e6477662c99016a9f37cafb76fc02
Arteminariy <artemborisoff.ab@gmail.com>
2023-06-28 14:28:54 +0300

    поправил отправку email

--- server/src/mail/mail.service.ts
+++ server/src/mail/mail.service.ts
@@ -1,29 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import * as nodemailer from 'nodemailer';
+import { MailerService } from '@nestjs-modules/mailer';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class MailService {
+  constructor(
+    private readonly mailerService: MailerService,
+    private readonly configService: ConfigService,
+  ) {}
   async sendActivationMail(email: string, link: string) {
-    const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: process.env.SMTP_PORT,
-      secure: false,
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASSWORD,
-      },
-    });
-    await transporter.sendMail({
-      from: process.env.SMTP_USER,
+    await this.mailerService.sendMail({
       to: email,
-      subject: `Активация аккаунта на ${process.env.CLIENT_URL}`,
+      subject: `Активация аккаунта на ${this.configService.get('CLIENT_URL')}`,
       text: '',
       html: `
-			<div>
-				<h1>Активация аккаунта ${email} на ${process.env.CLIENT_URL}</h1>
-				<a href="${link}">${link}</a>
-			</div>
-		`,
+				<div>
+					<h1>Ссылка для активации аккаунта на ${this.configService.get(
+            'CLIENT_URL',
+          )}</h1>
+					<a href="${link}">${link}</a>
+				</div>
+			`,
     });
   }
 }
